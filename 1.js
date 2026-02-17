d59e349fe9eda2792c1b49621c71941810db5489
Vishal Samyal <vishalsamyal61@gmail.com>
2026-02-17 16:44:56 +0530

    Add js files

---
+++ 1.js
@@ -0,0 +1,47 @@
+console.log("hello coder roots")
+
+// var a
+// a = 10
+// console.log(a)
+
+// let a 
+// a = 10
+// a = 20 
+// console.log(a)
+
+// const a = 10
+//  a = 20 // This line would cause an error because a const variable cannot be reassigned
+// console.log(a)
+
+// var a = 10
+// console.log(a)
+// const greet = () => {
+//     var a = 20
+// console.log(a)
+// }
+// greet()
+
+// console.log(a)
+
+// console.log(a)
+// var a = 10  //hoisting possible with var, but it will be undefined until the assignment is executed
+// let b = 20  //console.log(b) // This line would cause an error because b is not hoisted and cannot be accessed before its declaration
+// const c = 30 //console.log(c) // This line would also cause an error for the same reason as b
+
+// greet() // This will work because function declarations are hoisted
+// function greet() {
+//     console.log("Hello, World!")
+// }
+
+// // greet2() // This line would cause an error because greet2 is a function expression and is not hoisted
+// greet2()
+// const greet2 = function() {
+//     console.log("Hello, World!")
+// }
+
+// greet3() // This line would cause an error because greet3 is an arrow function expression and is not hoisted
+// let greet3 = () => {
+//     console.log("Hello, World!")
+// }
+// // greet3() // This line would also cause an error because greet3 is a function expression and is not hoisted
+ 
