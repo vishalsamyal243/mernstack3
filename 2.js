22a1eda14fe5006b421f61f689ea5fd54b4d0d4c
Vishal Samyal <vishalsamyal61@gmail.com>
2026-02-18 15:38:01 +0530

    add files

--- 2.js
+++ 2.js
@@ -1,1 +1,6 @@
-console.log("hello coder roots")
+ function changeContent() {
+          const head = document.getElementById("heading")
+          head.innerText = "Welcome to coder roots"
+ }
+
+ 
